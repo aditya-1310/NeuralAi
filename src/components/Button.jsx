b23433c30afbed9eabe0f61df63d5c1ca96429f2
Aditya Bhadade <bhadadeaditya1310@gmail.com>
2025-03-26 06:58:19 +0530

    changes done in button and header file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,27 @@
-import Buttonsvg from "../assets/svg/Buttonsvg";
-const Button = ({className , href, onClick , children , px, white}) => {
+import ButtonSvg from "../assets/svg/ButtonSvg";
 
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  
+  const spanClasses = "relative z-10";
 
-    const classes = `button relative infline-flex itmes-center justify-center h-11 transition-colors hover:text-color-1 
-    ${px || "px-7"} ${white ? "text-n-8 ": "text-n-1"} ${className || ""}`;
+  const renderButton = () => (
+    <button className={classes} onClick={onClick}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
 
-    const spanclasses = "relative z-10";
-    const renderButton = ()=>(
-        <button className={classes}>
-            <span className={spanclasses}>
-                {children}
-                {Buttonsvg(white)}
-            </span>
-        </button>
-    )
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
 
-    const renderLink = () =>(
-        <a href={href} className={classes}>
-            <span className={spanClasses}>{children}</span>
-            {ButtonSvg(white)}
-        </a>
-    )
-    return href?
-     renderLink():
-     renderButton;
-}
+  return href ? renderLink() : renderButton();
+};
 
-export default Button
+export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import {brainwave} from "../assets";
+import { navigation} from "../constants";
+import {useLocation} from 'react-router-dom';
+import Button from "./Button";
 const Header = () => {
+    const pathname = useLocation();
   return (
     <div className="fixed top-0 z-50 bg-n-8/90
     backdrop-blur-sm border-n-6 lg:bg-n-8/90lg:backdrop-blur-sm">
@@ -14,9 +18,25 @@ const Header = () => {
 
         <nav className="hiddren fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:max-auto lg:bg-transparent">
         <div className="relative z-2 flex flex-col item-center jsutify-center m-auto lg:flex-row">
-            1 2 3
+            {navigation.map((item=>(
+                <a key={item.id} href={item.url}
+                className={`block relative font-code text-2xl uppercase text-n-1
+                transition-color hover:text-color-1 ${item.onlyMobile ? "lg:hidden" :""} px-6  py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold
+                ${
+                    item.url === pathname.hash ? "z-2 lg:text-n-1"
+                     : "lg:text-n-1/50"} lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
+                    {item.title}
+                </a>
+            )))}
         </div>
         </nav>
+        <a href ="#signup"
+        className="button hidden mr-8 text-n-1/50 transition-color hover:text-n-1 lg:block"> NEW ACCOUNT
+        </a>
+        <Button
+        className="hidden lg:flex" href="#login">
+            Sign in
+        </Button>
             </div>
 
         Header
