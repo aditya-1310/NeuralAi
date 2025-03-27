1e2fa10002b86c9ae301b666f4fd328d54571ada
Aditya Bhadade <bhadadeaditya1310@gmail.com>
2025-03-27 20:11:43 +0530

    added the hero area and also added the generating.jsx and error are fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import  Button from "./components/Button"
 import Header from "./components/Header"
+import Hero from "./components/Hero";
 const App = ()=> {
 
   return (
@@ -9,6 +10,7 @@ const App = ()=> {
       <Header/>
        </div>
        <ButtonGradient/>
+      <Hero/>
     </>
   )
 }
